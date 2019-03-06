1f8298753dfed7f4c95ea6b20a18f43089d8b356
Ben Tallman <btallman@mscience.com>
2019-03-06 14:29:56 +0000

    Added auth to the web flow for statuspage so that everything requires login

--- app/Http/Routes/StatusPageRoutes.php
+++ app/Http/Routes/StatusPageRoutes.php
@@ -29,7 +29,7 @@ class StatusPageRoutes
      */
     public function map(Registrar $router)
     {
-        $router->group(['middleware' => ['web', 'ready', 'localize']], function (Registrar $router) {
+        $router->group(['middleware' => ['web', 'auth', 'ready', 'localize']], function (Registrar $router) {
             $router->get('/', [
                 'as'   => 'status-page',
                 'uses' => 'StatusPageController@showIndex',
